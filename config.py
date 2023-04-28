99aca593859a6fa31fbdda872b67ed44d7a1ff59
Stefan <stefan@akkelis.com>
2023-04-28 21:46:56 +0200

    postgres->postgresql

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost:5432/weightapp"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
